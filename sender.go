5511d948f174c7e195bbdf52618a03b8f8698b9f
audipitre <caltriestocode@gmail.com>
2025-06-14 15:42:56 +0100

    Send email

--- sender.go
+++ sender.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/smtp"
 	"os"
+	"os/exec"
+	"strconv"
 
 	dkim "github.com/toorop/go-dkim"
 )
 
-func send(body string) {
+func sendEmail(order Order) {
 	email := "sales@dealorean.co.uk"
 	options := dkim.NewSigOptions()
 	privateKey, err := os.ReadFile("dkim-private.key")
@@ -26,11 +29,9 @@ func send(body string) {
 	options.AddSignatureTimestamp = true
 	options.Canonicalization = "relaxed/relaxed"
 
-	pass := "READ FROM ENV"
+	pass := "SECURE THIS SOMEHOW"
 
-	recipient := ""
-
-	msg := createEmailBody(recipient)
+	msg := createEmailBody(order)
 	err = dkim.Sign(&msg, options)
 	if err != nil {
 		log.Fatal("DKIM signing failed:", err)
@@ -39,7 +40,7 @@ func send(body string) {
 	auth := smtp.PlainAuth("", email, pass, "smtp.gmail.com")
 	err = smtp.SendMail("smtp.gmail.com:587", auth,
 		email,
-		[]string{recipient},
+		[]string{"lewfog@gmail.com"},
 		msg)
 
 	if err != nil {
@@ -49,16 +50,37 @@ func send(body string) {
 	log.Println("Email sent with DKIM signature.")
 }
 
-func createEmailBody(recipient string) []byte {
+func createEmailBody(order Order) []byte {
 	var body bytes.Buffer
 
 	body.WriteString("From: Dealorean <sales@dealorean.co.uk>\r\n")
-	body.WriteString(fmt.Sprintf("To: %s\r\n", recipient))
+	body.WriteString(fmt.Sprintf("To: %s\r\n", "lewfog@gmail.com"))
 	body.WriteString("Subject: Your invoice sent from the future\r\n")
 	body.WriteString("MIME-Version: 1.0\r\n")
-	body.WriteString("Content-Type: text/plain; charset=UTF-8\r\n")
+	body.WriteString("Content-Type: text/html; charset=UTF-8\r\n")
 	body.WriteString("\r\n")
-	body.WriteString("Hello Lewis\r\nInvoice goes here, but not generated it yet.\r\n")
+
+	orderId := strconv.FormatInt(order.OrderID, 10)
+
+	itemJsonData, _ := json.Marshal(order.ItemsBought)
+	itemJsonString := string(itemJsonData)
+	deliveryPrice := strconv.FormatFloat(order.DeliveryPrice, 'f', 2, 32)
+	protectionPrice := strconv.FormatFloat(order.ProtectionPrice, 'f', 2, 32)
+	totalPrice := strconv.FormatFloat(order.TotalPrice, 'f', 2, 32)
+	cmd := exec.Command("node", "/home/audipitre/repos/LewisAutoInvoicer/mjml-email-gen/gen-html.js", orderId, order.Date, order.CustomerUsername, itemJsonString, deliveryPrice, protectionPrice, totalPrice)
+
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+
+	if err != nil {
+		log.Println(stderr.String())
+		log.Fatalf("Error executing command: %v", err)
+	}
+	body.WriteString(out.String())
 
 	return body.Bytes()
 }
